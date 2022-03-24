ab15dd4cea1ac0fa197c5cc790de9c8d26feb2c3
warmCabin <ryanwb2@gmail.com>
2022-03-24 11:11:36 -0400

    major cleanup so it only does precisely what's used

--- item1_numberer.lua
+++ item1_numberer.lua
@@ -17,69 +17,18 @@
     It even draws a smiley face when you have a good subpixel for the zip!
 ]]
 
---Only interested in tracking the 4 weapon-controllable ones ($0592 - $0595).
---$0596 affects one of the Crash Bomb explosion particles, but $0596 - $059F
---pretty much stay at 0 forever. Change NUM_HITBOXES to 16 if you're curious!
-local NUM_HITBOXES = 4
-local EmuYFix = 0
-
-local function drawBox(x1,y1,x2,y2,c1,c2)
-	gui.box(x1, y1+EmuYFix, x2, y2+EmuYFix, c1, c2)
-end
-
-local function drawText(x,y,t,c1,c2)
-	gui.text(x, y+EmuYFix, t, c1, c2 )
-end
-
---synaesthesia for hitbox sizes. Redder = bigger.
---format: fill color, outline color
-local hitSizeColors = {
-{"#00FF0040","green"},
-{"#0000FF40","blue"},
-{"#FFEF0040","#FFEF00"},
-{"#FF7F0040","#FF7F00"},
-{"#FF000040","red"}
-}
-setmetatable(hitSizeColors, {__index=function(t,k)
-	return {"#FFFFFF40", "white"} --all glitchy out-of-bounds hitboxes are drawn white.
-end})
-
---Written by finalfighter.
---Variables renamed by me for clarity.
---I also removed the delay scroll tracking and
---HP/sprite timer stuff, and added two lines to draw the
---hitbox types at $059X. It's a lightweight hitbox function!
+-- draw coordinate computation logic by finalfighter
 local function drawSpriteInfo()
-
-	-- local weapon = memory.readbyte(0xA9)
-	-- if weapon ~= 8 then return end
-
 	local scX = memory.readbyte(0x20)*256+memory.readbyte(0x1F)
 	local scY = 0
 
-	for i=2,8 do
+	for i = 2, 8 do
 		local x = memory.readbyte(0x0440+i)*256+memory.readbyte(0x0460+i) - scX
 		local y = memory.readbyte(0x04A0+i)                               - scY
-
 		local flags = memory.readbyte(0x0420+i)
-		if flags>=0x80 then --sprite is alive
-		
-			if i<0x10 and i~=1 then --Rockman & projectiles
-				local propIndex = memory.readbyte(0x0590+i)           --weapon hitbox type (0-4)
-				local tmp = memory.readbyte(0xD4DC+propIndex)         --offset into property tables
-				local hitSizeX = memory.readbyte(0xD4E1+tmp)-0xC  + 4 --read from property tables
-				local hitSizeY = memory.readbyte(0xD581+tmp)-0x14 + 4
-				local bg,ol = hitSizeColors[propIndex+1][1],hitSizeColors[propIndex+1][2]
-				
-				-- drawBox(x-hitSizeX, y-hitSizeY, x+hitSizeX, y+hitSizeY, bg, ol)
-				drawText(x - 2, y - 2, 5 - i, "white", "black") -- slots 2, 3, 4? 5 - i
-			else --regular entities
-				local tmp = memory.readbyte(0x06E0+i)
-				local hitSizeX = math.max(0, memory.readbyte(0xD501+tmp) - 4 )
-				local hitSizeY = math.max(0, memory.readbyte(0xD5A1+tmp) - 4 )
-				drawBox(x-hitSizeX, y-hitSizeY, x+hitSizeX, y+hitSizeY, "#FFFFFF40", "white")
-			end
-			
+        
+		if flags>=0x80 then -- sprite is alive
+            gui.text(x - 2, y - 2, 5 - i, "white", "black") -- slots 2, 3, 4? 5 - i		
 		end
 	end
 end
@@ -91,11 +40,10 @@ local function postFrame()
     local theSubpixel = memory.readbyte(0x04C3)
     -- drawText(5, 15, "the subpixel: "..theSubpixel)
     if (theSubpixel > 128) then
-        drawText(5, 15, ":)")
+        gui.text(5, 15, ":)")
     else
-        drawText(5, 15, ":(")
+        gui.text(5, 15, ":(")
     end
-	-- drawText(5,15,str,"white","0000FF80")
 	
 end
 emu.registerafter(postFrame)
