fdd5d8d39633d5f6d5866febb9ebbf225c45055d
warmCabin <ryanwb2@gmail.com>
2022-03-24 02:04:01 -0400

    update comments and stuff

--- solid_sprite_visualize.lua
+++ solid_sprite_visualize.lua
@@ -1,34 +1,14 @@
+--[[
+    Draw the solid sprite collision override data to screen.
+    mm2_fullmap draws the little blue squares, but it can only understand block-aligned masks (i.e. lower nyble = 0).
+    Block-aligned masks are really the intended purpose of this system, but...you can get weird with it >:)
+]]
+
 local SOLID_SPRITE_TYPE_ADDRESS = 0x04FB
 local solidSpriteType = 0
 
 local prevSelect = false
 local shouldForceTileType = true
-
-print(rom.getfilename())
-
-local charMap = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'}
-
-local function toString(num, base, length)
-    
-    base = base or 10
-    
-    if not num or base < 2 or base > 16 then return end
-    
-    if num == 0 then return ("0"):rep(length) end
-    
-    if num < 0 then return "-"..toString(-num, base, length) end
-    
-    local ret = ""
-
-    while num > 0 do
-        ret = charMap[num % base + 1]..ret
-        num = math.floor(num / base)
-    end
-    
-    if length then ret = (("0"):rep(length - #ret))..ret end
-    
-    return ret
-end
 
 local function forceTileType()
     if joypad.get(1).select then
