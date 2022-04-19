ba7867a32df97db186010d06be5021e9c97e66d4
warmCabin <ryanwb2@gmail.com>
2022-04-18 21:42:16 -0400

    add and show names for a bunch of sprites

--- sprite_table.lua
+++ sprite_table.lua
@@ -15,6 +15,97 @@ local showHex = not arg:find("--decimal-pos", 0, true)
 local TABLE_BACKGROUND_COLOR = "#13067FC0"
 local TABLE_TEXT_COLOR = "FFFFFFC0"
 
+local playerNames = {
+    [0x23] = "Lemon",
+    [0x24] = "Dust",
+    [0x25] = "Meatball",
+    [0x33] = "Bubble Lead",
+    [0x34] = "Quick Boomerang",
+    [0x36] = "Metal Blade",
+    [0x39] = "Item 2",
+    [0x3A] = "Item 3",
+    [0x3C] = "Quick Boomerang (dead)",
+    [0x3F] = "Splash",
+}
+setmetatable(playerNames, {__index = function(t, k) return string.format("$%02X", k) end})
+
+local enemyNames = {
+    [0x00] = "Shrimp A",
+    [0x01] = "Shrimp B",
+    [0x02] = "Anko fins?",
+    [0x03] = "Metroid spawner", -- Mother Brain? Idk I've never played Metroid.
+    [0x04] = "Metroid",
+    [0x06] = "Poof",
+    [0x08] = "Crab",
+    [0x07] = "Crab spawner",
+    [0x0A] = "Hermit crab",
+    [0x0B] = "Hermit crab shell",
+    [0x0C] = "Mama frog",
+    [0x0D] = "Baby frog",
+    [0x0E] = "Bubble",
+    [0x0F] = "Anko",
+    [0x10] = "Anko eye?",
+    [0x12] = "Track platform",
+    [0x13] = "Falling platform",
+    [0x14] = "Quick laser manager",
+    [0x15] = "Quick laser",
+    [0x21] = "Telly spanwer", -- AKA "Terry comes out"
+    [0x22] = "Telly",
+    [0x23] = "Hothead",
+    [0x25] = "Light switch 1 (off)",
+    [0x26] = "Light switch 2 (on)",
+    [0x27] = "Light switch 1 (on)",
+    [0x28] = "Light switch 2 (off)",
+    [0x2C] = "Prop-top",
+    [0x2B] = "Prop-top spanwer",
+    [0x2D] = "Crash wall (tall)",
+    [0x2F] = "Door",
+    [0x30] = "Crusher",
+    [0x31] = "Blocky head",
+    [0x32] = "Blocky body",
+    [0x33] = "Blocky can",
+    [0x35] = "Sniper Joe bullet",
+    [0x3C] = "Kopipi",
+    [0x3E] = "Cloud platform",
+    [0x46] = "Springer",
+    [0x47] = "Drill spawner",
+    [0x48] = "Drill (up)",
+    [0x49] = "Drill (down)",
+    [0x4B] = "Shotman (left)",
+    [0x4C] = "Shotman (right)",
+    [0x4D] = "Shotman bullet",
+    [0x4E] = "Sniper Armor",
+    [0x4F] = "Sniper Joe",
+    [0x50] = "Scworm dispenser",
+    [0x51] = "Scworm",
+    [0x53] = "Yoku block A",
+    [0x54] = "Yoku block B",
+    [0x55] = "Yoku block C",
+    [0x57] = "Crash wall (short)",
+    [0x58] = "Flame", -- Boss value. Reused?
+    [0x59] = "Quick Man's boomerang", -- Boss value. Reused?
+    [0x5A] = "Bubble bullet",
+    [0x5B] = "Bubble Lead", -- Boss value
+    [0x60] = "Meatball",
+    [0x76] = "Large health drop",
+    [0x77] = "Small health drop",
+    [0x78] = "Large energy drop",
+    [0x79] = "Small energy drop",
+    [0x7A] = "E tank",
+    [0x7B] = "Extra life",
+}
+setmetatable(enemyNames, {__index = function(t, k) return string.format("$%02X", k) end})
+
+local bossNames = {
+    [0x00] = "Heat Man",
+    [0x01] = "Air Man",
+    [0x02] = "Wood Man",
+    [0x03] = "Bubble Man",
+    [0x04] = "Quick Man",
+    [0x05] = "Flash Man",
+}
+setmetatable(bossNames, {__index = function(t, k) return string.format("$%02X", k) end})
+
 local function drawBox(x1, y1, x2, y2, c1, c2)
 	gui.box(x1, y1 + EmuYFix, x2, y2 + EmuYFix, c1, c2)
 end
@@ -33,7 +124,7 @@ local hitSizeColors = {
     {"#FF000040","red"}
 }
 setmetatable(hitSizeColors, {__index=function(t,k)
-	return {"#FFFFFF40", "white"} -- The default hitboxe color is white.
+	return {"#FFFFFF40", "white"} -- The default hitbox color is white.
 end})
 
 local function formatPos(pixel, subpixel)
@@ -44,6 +135,22 @@ end
 
 local function getCoords(tableIndex)
     return math.floor(tableIndex / 16) * 132, (tableIndex % 16) * 10 + 20
+end
+
+local function getPlayerName(slot, id)
+    if slot == 0 then
+        return "Mega Man"
+    else
+        return playerNames[id]
+    end
+end
+
+local function getEnemyName(slot, id)
+    if slot == 1 then
+        return bossNames[memory.readbyte(0xB3)]
+    else
+        return enemyNames[id]
+    end
 end
 
 -- Written by finalfighter.
@@ -67,7 +174,8 @@ local function drawSpriteInfo()
         local ySub = memory.readbyte(0x04C0 + i)
 		local flags = memory.readbyte(0x0420 + i)
         local index = memory.readbyte(0x0400 + i)
-        local timer = memory.readbyte(0x04E0+i)
+        local timer = memory.readbyte(0x04E0 + i)
+        local health = memory.readbyte(0x06C0 + i)
         
         local drawX = memory.readbyte(0x0440 + i) * 256 + xPix - scX
 		local drawY = yPix - scY
@@ -92,9 +200,11 @@ local function drawSpriteInfo()
                 
                 if drawTable then
                     drawText(tableDrawX, tableDrawY,
-                        string.format("%02X: %02X (%s,%s)", i, index, formatPos(xPix, xSub), formatPos(yPix, ySub)), TABLE_TEXT_COLOR, TABLE_BACKGROUND_COLOR)
+                        -- string.format("%02X: %02X (%s,%s)", i, index, formatPos(xPix, xSub), formatPos(yPix, ySub)),
+                        string.format("%02X: %s", i, getPlayerName(i, index)),
+                        TABLE_TEXT_COLOR, TABLE_BACKGROUND_COLOR)
                 end
-			else -- regular entities
+			else -- Enemies
 				local tmp = memory.readbyte(0x06E0 + i)
 				local hitSizeX = math.max(0, memory.readbyte(0xD501 + tmp) - 4 )
 				local hitSizeY = math.max(0, memory.readbyte(0xD5A1 + tmp) - 4 )
@@ -107,7 +217,10 @@ local function drawSpriteInfo()
                 -- drawText(drawX - 5, drawY - hitSizeY - 9, string.format("%d", timer))
                 if drawTable then
                     drawText(tableDrawX, tableDrawY,
-                        string.format("%02X: %02X (%s,%s)", i, index, formatPos(xPix, xSub), formatPos(yPix, ySub)), TABLE_TEXT_COLOR, TABLE_BACKGROUND_COLOR)
+                        string.format("%02X: %s", i, getEnemyName(i, index)),
+                        -- string.format("%02X: %02X %02X %02X", i, index, health, flags),
+                        -- string.format("%02X: %02X (%s,%s)", i, index, formatPos(xPix, xSub), formatPos(yPix, ySub)),
+                        TABLE_TEXT_COLOR, TABLE_BACKGROUND_COLOR)
                 end
 			end
 		else 
